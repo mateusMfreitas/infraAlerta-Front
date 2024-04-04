27332fcf3627dd8a77d7e9e3953852d34aa42c62
Lucas Fragoso <88888171+lu-fragoso@users.noreply.github.com>
2024-04-04 00:08:12 -0300

    Adiciona rota para redefinição de senha

--- src/routes.js
+++ src/routes.js
@@ -2,6 +2,7 @@ import {BrowserRouter, Routes, Route} from "react-router-dom";
 import Login from "./login/login";
 import Register from "./register/register";
 import Problema from "./review/problema";
+import ForgotPassword from "./forgetPassword/forgotPassword";
 
 function AppRoutes() {
     return(
@@ -10,6 +11,7 @@ function AppRoutes() {
                 <Route path="/login" element={<Login/>} />
                 <Route path="/register" element={<Register/>} />
                 <Route path="/problema" element={<Problema/>} />
+                <Route path="/forgot-password" element={<ForgotPassword/>} />
             </Routes>
         </BrowserRouter>
     );
