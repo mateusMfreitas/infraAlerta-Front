6196910b2b796cc4c647fb4fc1a79bd3ecad8246
GuilhermeGBH <guilherme2708@hotmail.com>
2024-04-30 20:31:53 -0300

    Adicionado bootstrap

--- src/index.js
+++ src/index.js
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
 import AppRoutes from './routes';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
