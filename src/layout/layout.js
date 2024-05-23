d864ba8f568875a42e25719efce24c345694aadc
Lucas Fragoso <88888171+lu-fragoso@users.noreply.github.com>
2024-05-23 20:16:22 -0300

    Adicionar redirecionamento no menu lateral

--- src/layout/layout.js
+++ src/layout/layout.js
@@ -2,25 +2,23 @@ import React, {useState,useEffect} from 'react';
 import NavBar from '../navBar/navBar'; // Importe o NavBar
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faHouse, faMagnifyingGlass, faTriangleExclamation, faSheetPlastic } from '@fortawesome/free-solid-svg-icons';
-import { useNavigate } from 'react-router-dom';
+import { useNavigate, useLocation } from 'react-router-dom';
 import './layout.css'
 
 const Layout = ({ children }) => {
-    const [selectedMenu, setSelectedMenu] = useState('Home');
+    const [selectedMenu, setSelectedMenu] = useState();
+    const location = useLocation();
     const navigate = useNavigate();
 
     const redirecionarInserirChamado = () => {
         navigate('/problem'); 
-        setSelectedMenu('Problem');
     };
 
     const redirecionarInicio = () => {
         navigate('/report'); 
-        setSelectedMenu('Home');
     };
     const redirecionarRelatorio = () => {
         navigate('/graphics'); 
-        setSelectedMenu('Relatorio');
     };
 
     return (
@@ -32,17 +30,17 @@ const Layout = ({ children }) => {
                     <h4 className='m-3 title-menu'>Menu Principal</h4>
                     <div className='line text-center'>
             <span className='button-menu'>
-                <button onClick={redirecionarInicio} className={selectedMenu === 'Home' ? 'active' : 'button-menu'}>
+                <button onClick={redirecionarInicio} className={`layout-button ${location.pathname === '/report' ? 'selected' : ''}`}>
                     <FontAwesomeIcon icon={faHouse} className='mr-2 icon' />Home
                 </button>
             </span>
             <span className='button-menu'>
-                <button onClick={redirecionarInserirChamado} className={selectedMenu === 'Problem' ? 'button-menu active' : '  button-menu '}>
+                <button onClick={redirecionarInserirChamado} className={`layout-button ${location.pathname === '/problem' ? 'selected' : ''}`}>
                 <FontAwesomeIcon icon={faTriangleExclamation} className='mr-2 icon' />Adicionar Relato
                 </button>
             </span>
             <span className='button-menu'>
-                <button onClick={redirecionarRelatorio} className={selectedMenu === 'Relatorio' ? 'button-menu active' : 'button-menu'}>
+                <button onClick={redirecionarRelatorio} className={`layout-button ${location.pathname === '/graphics' ? 'selected' : ''}`}>
                 <FontAwesomeIcon icon={faSheetPlastic} className='mr-2 icon' />Relatórios
                 </button>
             </span>
