029e288cf665556aac01d2ede74439fd14f813db
Lucas Fragoso <88888171+lu-fragoso@users.noreply.github.com>
2024-05-23 01:25:46 -0300

    Adicionar redirecionamento no menu lateral

--- src/layout/layout.js
+++ src/layout/layout.js
@@ -1,18 +1,26 @@
-import React from 'react';
+import React, {useState,useEffect} from 'react';
 import NavBar from '../navBar/navBar'; // Importe o NavBar
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faHouse, faMagnifyingGlass, faTriangleExclamation, faSheetPlastic } from '@fortawesome/free-solid-svg-icons';
 import { useNavigate } from 'react-router-dom';
+import './layout.css'
 
 const Layout = ({ children }) => {
+    const [selectedMenu, setSelectedMenu] = useState('Home');
     const navigate = useNavigate();
 
     const redirecionarInserirChamado = () => {
         navigate('/problem'); 
+        setSelectedMenu('Problem');
     };
 
     const redirecionarInicio = () => {
         navigate('/report'); 
+        setSelectedMenu('Home');
+    };
+    const redirecionarRelatorio = () => {
+        navigate('/graphics'); 
+        setSelectedMenu('Relatorio');
     };
 
     return (
@@ -23,20 +31,22 @@ const Layout = ({ children }) => {
                 <div className='menuLateral col-md-3' id='menuLateral'>
                     <h4 className='m-3 title-menu'>Menu Principal</h4>
                     <div className='line text-center'>
-                        <span className='button-menu'>
-                            <a onClick={redirecionarInicio}>
-                                <FontAwesomeIcon icon={faHouse} className='mr-2 icon' />Home
-                            </a>
-                        </span>
-                        <span className='button-menu'>
-                            <a onClick={redirecionarInserirChamado}>
-                            <FontAwesomeIcon icon={faTriangleExclamation} className='mr-2 icon' />Adicionar Relato
-                            </a>
-                        </span>
-                        <span className='button-menu'>
-                            <FontAwesomeIcon icon={faSheetPlastic} className='mr-2 icon' />Relatórios
-                        </span>
-                    </div>
+            <span className='button-menu'>
+                <button onClick={redirecionarInicio} className={selectedMenu === 'Home' ? 'active' : 'button-menu'}>
+                    <FontAwesomeIcon icon={faHouse} className='mr-2 icon' />Home
+                </button>
+            </span>
+            <span className='button-menu'>
+                <button onClick={redirecionarInserirChamado} className={selectedMenu === 'Problem' ? 'button-menu active' : '  button-menu '}>
+                <FontAwesomeIcon icon={faTriangleExclamation} className='mr-2 icon' />Adicionar Relato
+                </button>
+            </span>
+            <span className='button-menu'>
+                <button onClick={redirecionarRelatorio} className={selectedMenu === 'Relatorio' ? 'button-menu active' : 'button-menu'}>
+                <FontAwesomeIcon icon={faSheetPlastic} className='mr-2 icon' />Relatórios
+                </button>
+            </span>
+        </div>
                     <div className='line text-center'>
                         <h1>Preferências</h1>
                         <h1>Configurações</h1>
