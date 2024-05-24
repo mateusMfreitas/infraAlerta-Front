bae6eb9789b1732922c92fd3979cb40d0e41e2d1
Mateus <mateusmfreitas12@gmail.com>
2024-05-23 21:24:57 -0300

    Bloqueando acesso de usuários não logados

--- src/auth/auth.js
+++ src/auth/auth.js
@@ -1,14 +1,16 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
 
 export function withAuth(Component) {
-    const navigate = useNavigate();
-
   return function ProtectedRoute(props) {
+    const navigate = useNavigate();
     const userString = sessionStorage.getItem('user');
-    if (!userString) {
-      navigate('/login');
-    }
+
+    useEffect(() => {
+      if (!userString) {
+        navigate('/login');
+      }
+    }, [navigate, userString]);
 
     return <Component {...props} />;
   }
--- src/routes.js
+++ src/routes.js
@@ -11,20 +11,28 @@ import Graphics from "./graphics/graphics";
 import LandingPage from "./landingPage/landingPage";
 import { withAuth } from "./auth/auth";
 
+const ProtectedProblem = withAuth(Problem);
+const ProtectedReport = withAuth(Report);
+const ProtectedReportDetails = withAuth(ReportDetails);
+const ProtectedUDashboard = withAuth(UDashboard);
+const ProtectedADashboard = withAuth(ADashboard);
+const ProtectedGraphics = withAuth(Graphics);
+
+
 function AppRoutes() {
     return(
         <BrowserRouter>
             <Routes>
-                <Route path="/login" element={<Login/>} />
+            <Route path="/login" element={<Login/>} />
                 <Route path="/" element={<LandingPage/>} />
                 <Route path="/register" element={<Register/>} />
-                <Route path="/problem" element={withAuth(<Problem/>)} />
+                <Route path="/problem" element={<ProtectedProblem/>} />
                 <Route path="/forgot-password" element={<ForgotPassword/>} />
-                <Route path="/report" element={withAuth(<Report/>)} />
-                <Route path="/report/:id" element={withAuth(<ReportDetails/>)} />
-                <Route path="/uDashboard" element={withAuth(<UDashboard/>)} />
-                <Route path="/aDashboard" element={withAuth(<ADashboard/>)} />
-                <Route path="/graphics" element={withAuth(<Graphics/>)}/>
+                <Route path="/report" element={<ProtectedReport/>} />
+                <Route path="/report/:id" element={<ProtectedReportDetails/>} />
+                <Route path="/uDashboard" element={<ProtectedUDashboard/>} />
+                <Route path="/aDashboard" element={<ProtectedADashboard/>} />
+                <Route path="/graphics" element={<ProtectedGraphics/>}/>
             </Routes>
         </BrowserRouter>
     );
